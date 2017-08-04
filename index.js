c6c3f3d16aea1586e824a45a2752f2ce7c62523c
Robert Wieland <rwieland92@gmail.com>
2017-08-04 16:12:33 -0400

    Added partial skeleton to index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+var Game = function() {
+	
+}
+
+Game.prototype.createGameElement = function() {
+	
+}
+
+Game.prototype.removeGameElement = function() {
+	
+}
+
+Game.prototype.hideGameElement = function() {
+	
+}
+
+Game.prototype.resizeGameElement = function() {
+	
+}
+
+Game.prototype.recolorGameElement = function() {
+	
+}
+
+var Menu = function() {
+	this.head = ''
+	this.tree = {}
+}
+
+Menu.prototype.createMenuElement = function() {
+	
+}
+
+Menu.prototype.removeMenuElement = function() {
+	
+}
+
+Menu.prototype.updateMenuElement = function() {
+	
+}
+
+Menu.prototype.hideMenuElement = function() {
+	
+}
+
+Menu.prototype.addOption = function() {
+	
+}
+
+Menu.prototype.deleteOption = function() {
+	
+}
+
